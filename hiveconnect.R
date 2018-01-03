5f7123caf9ea8d6d80953d2371ccd358a0b3af67
jinzhao <zjin2008@126.com>
2018-01-03 13:54:08 +0800

    hivconnect

---
+++ hiveconnect.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(plotly)
+library(ggedit)
+options(scipen =  200)
+data1 <- read.csv('~/Desktop/hist.csv')
+chart <- filter(data1,t1.cd < 50 )%>% ggplot(aes(x = t1.cd,y = g,colour = as.factor(t1.dt))) + geom_line()
+p <- ggplotly(chart)
+p
+ggedit(chart)
+library(RJDBC)
+drv <- JDBC('org.apache.hive.jdbc.HiveDriver',list.files("~/Documents/software/apache-hive-1.2.2-bin/lib",
+                   pattern = "jar$", full.names=TRUE, recursive=TRUE))
+conn<-dbConnect(drv, sprintf('jdbc:hive2://192.168.2.47/:8888/default'), 'jinzhao', 'mp!jinzhao')
